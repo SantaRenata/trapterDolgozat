dc8367567c6d95e432461b930a6768f47863021d
SantaRenata <renatasanta2002@gmail.com>
2022-10-26 15:48:00 +0200

    Számítás megoldva

---
+++ src/app.js
@@ -0,0 +1,26 @@
+var szamolGomb = document.querySelector("#szamolGomb");
+var eredmeny = document.querySelector("#eredmeny");
+var aoldal = document.querySelector("#aoldal");
+var boldal = document.querySelector("#boldal");
+var coldal = document.querySelector("#coldal");
+var doldal = document.querySelector("#doldal");
+szamolGomb.addEventListener("click", function () {
+    var aoldalNum = Number(aoldal.value);
+    var boldalNum = Number(boldal.value);
+    var coldalNum = Number(coldal.value);
+    var doldalNum = Number(boldal.value);
+    var area = szamolTerulet(aoldalNum, boldalNum, coldalNum, doldalNum);
+    eredmeny.textContent = "Terület: " + String(area);
+    aoldal.value = " ";
+    boldal.value = " ";
+    coldal.value = " ";
+    doldal.value = " ";
+});
+function szamolTerulet(aoldal, boldal, coldal, doldal) {
+    var terulet = (aoldal + coldal) / (4 * (aoldal - coldal)) *
+        Math.sqrt((aoldal + boldal - coldal + doldal) *
+            (aoldal - boldal - coldal + doldal) *
+            (aoldal + boldal - coldal - doldal) *
+            (-aoldal + boldal + coldal + doldal));
+    return terulet;
+}
---
+++ src/app.ts
@@ -0,0 +1,30 @@
+const szamolGomb = document.querySelector("#szamolGomb");
+const eredmeny = document.querySelector("#eredmeny");
+const aoldal = document.querySelector("#aoldal") as HTMLInputElement;
+const boldal = document.querySelector("#boldal") as HTMLInputElement;
+const coldal = document.querySelector("#coldal") as HTMLInputElement;
+const doldal = document.querySelector("#doldal") as HTMLInputElement;
+
+szamolGomb.addEventListener("click", () => {
+    let aoldalNum: number = Number(aoldal.value);
+    let boldalNum: number = Number(boldal.value);
+    let coldalNum: number = Number(coldal.value);
+    let doldalNum: number = Number(boldal.value);
+    let area = szamolTerulet(aoldalNum,boldalNum,coldalNum, doldalNum);
+    eredmeny.textContent = "Terület: " + String(area);
+    aoldal.value = " ";
+    boldal.value = " ";
+    coldal.value = " ";
+    doldal.value = " ";
+});
+
+function szamolTerulet(aoldal:number, boldal:number, coldal:number, doldal:number):number{
+    let terulet = (aoldal+coldal)/(4*(aoldal-coldal))*
+    Math.sqrt(
+        (aoldal+boldal-coldal+doldal)*
+        (aoldal-boldal-coldal+doldal)*
+        (aoldal+boldal-coldal-doldal)*
+        (-aoldal+boldal+coldal+doldal)
+        );
+    return terulet;
+}
